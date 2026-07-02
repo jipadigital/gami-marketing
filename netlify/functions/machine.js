6a39c2f1bf40d5b35a2cf620e002f9fd144c6438
jipadigital <jipadigital@gmail.com>
2026-07-02 13:54:47 -0400

    debug: expoe credenciais_origem/lengths tbm no sucesso (comparar cidade OK)

--- netlify/functions/machine.js
+++ netlify/functions/machine.js
@@ -338,6 +338,8 @@ exports.handler = async function(event){
       total: todos.length,
       paginas_lidas: pagina,
       truncado: truncado,
+      credenciais_origem: credenciaisOrigem,
+      user_len: (user||'').length, pass_len: (pass||'').length, apikey_len: (apiKey||'').length,
       filtro_data: (dataInicio||dataFim) ? {inicio:dataInicio, fim:dataFim, aplicado:filtroAplicado} : null,
       atualizado_em: new Date(agora).toISOString(),
       response: todos
